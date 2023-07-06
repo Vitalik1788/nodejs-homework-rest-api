2ecd526fc7566c9cd22ceeed69484854451d1961
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-07-06 19:11:13 +0300

    conect server

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  'mongodb+srv://Vitalii:0lQorZNXL2GLHYPl@cluster0.wlrsawm.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000)
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
