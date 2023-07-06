ca05af592a110abfe9d22310b00c4e38a8506678
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-07-06 21:05:40 +0300

    update gitignor

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const app = require('./app');
+const { DB_HOST, PORT } = process.env;
 
-const DB_HOST =
-  'mongodb+srv://Vitalii:0lQorZNXL2GLHYPl@cluster0.wlrsawm.mongodb.net/db-contacts?retryWrites=true&w=majority';
 
 mongoose.connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000)
+    app.listen(PORT);
   })
   .catch(error => {
     console.log(error.message);
