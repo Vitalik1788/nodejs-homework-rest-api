f1f3a4a09b2567aee8d59b77f74a56410b32fe64
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-07-15 11:31:54 +0300

    update HW04

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,16 +4,20 @@ const { controllerWrapper, HttpError } = require('../helpers');
 
 const onGetAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
+  const { page = 1, limit = 10, favorite } = req.query;
+  const skip = (page - 1) * limit;  
   const result = await Contact.find({ owner }, '-createdAt', {
     skip,
     limit,
   }).populate('owner', 'email subscription');
-  if (!result) {
-    throw HttpError(404, 'Not found');
+  if (!favorite) {
+    res.json(result);
+  } else if (favorite) {
+    const favoriteContacts = result.filter(contact => contact.favorite === true);
+    res.json(favoriteContacts);
   }
-  res.json(result);
+
+  
 };
 
 const onGetContactById = async (req, res) => {
